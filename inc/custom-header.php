35dc098056e4d6d898edaba98f19fdc75d60a70a
m-e-h <info@martyhelmick.com>
2016-06-07 12:48:54 -0400

    custom headers

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -2,14 +2,10 @@
 /**
  * Handles the setup and usage of the WordPress custom headers feature.
  *
- * @package    Stargazer
- * @author     Justin Tadlock <justin@justintadlock.com>
- * @copyright  Copyright (c) 2013 - 2016, Justin Tadlock
- * @link       http://themehybrid.com/themes/abraham
- * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @package  RCDOC
  */
 
-# Call late so child themes can override.
+// Call late so child themes can override.
 add_action( 'after_setup_theme', 'abraham_custom_header_setup', 15 );
 
 /**
@@ -32,7 +28,7 @@ function abraham_custom_header_setup() {
 			'default-text-color'     => 'ffffff',
 			'header-text'            => true,
 			'uploads'                => true,
-			'wp-head-callback'       => 'abraham_custom_header_wp_head'
+			'wp-head-callback'       => 'abraham_custom_header_wp_head',
 		)
 	);
 }
@@ -45,18 +41,40 @@ function abraham_custom_header_setup() {
  * @return void
  */
 function abraham_custom_header_wp_head() {
-$style = '';
-if ( display_header_text() ) {
-	$hex = get_header_textcolor();
-	if ( ! $hex )
-		return;
+	$style = '';
+	if ( display_header_text() ) {
+		$hex = get_header_textcolor();
+		if ( ! $hex ) {
+			return; }
 
-	$style .= ".site-header,.archive-header{color:#{$hex};}";
-}
-if (get_header_image()) {
-	$bg_image = get_header_image();
-	$style .= ".archive-header{background-image:url({$bg_image});}.page-head-text{min-height:20vw;}";
- }
+		$style .= ".site-header,.archive-header{color:#{$hex};}";
+	}
 
+	global $cptarchives;
+	$queried_object = get_queried_object_id();
+	$term_image = get_term_meta( $queried_object, 'image', true );
+	if ( $term_image ) {
+		$bg_image = wp_get_attachment_image_url( $term_image, 'abe-hd' );
+
+	} elseif ( has_post_thumbnail( $cptarchives->get_archive_id() ) ) {
+		$bg_image = wp_get_attachment_image_url( get_post_thumbnail_id( $cptarchives->get_archive_id() ), 'abe-hd' );
+
+	} elseif ( has_post_thumbnail() ) {
+		$bg_image = wp_get_attachment_image_url( get_post_thumbnail_id(), 'abe-hd' );
+
+	} elseif ( get_header_image() ) {
+		$bg_image = get_header_image();
+	}
+	$style .= ".article-hero{background-image:url({$bg_image});}";
 	echo "\n" . '<style type="text/css" id="custom-header-css">' . trim( $style ) . '</style>' . "\n";
+}
+
+
+add_action( 'tha_header_after', 'doc_article_hero' );
+
+function doc_article_hero() {
+	if ( is_front_page() ) {
+		return; }
+
+	echo '<div id="article-hero" class="article-hero u-1of1 u-bg-center u-bg-no-repeat u-bg-cover u-tinted-image u-bg-fixed u-abs u-left0 u-right0"></div>';
 }
