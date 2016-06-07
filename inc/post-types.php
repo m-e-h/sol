eff222c98d7a268629f2ceb2c7d4f8eb6925a844
m-e-h <info@martyhelmick.com>
2016-06-06 22:01:37 -0400

    add post-types without help

--- inc/post-types.php
+++ inc/post-types.php
@@ -15,45 +15,87 @@ add_action( 'init', 'sol_register_post_types' );
  */
 function sol_register_post_types() {
 
-	$sol_page_supports = array(
-		'title',
-		'editor',
-		'author',
-		'thumbnail',
-		'excerpt',
-		'page-attributes',
-		'theme-layouts',
-		'archive',
-		'zoninator_zones'
+	register_post_type(
+        'eng_sol',
+    	array(
+    		'label'                 => __( 'Engineering Solutions', 'sol' ),
+    		'labels'                => array(
+        		'name'                  => _x( 'Engineering Solutions', 'Post Type General Name', 'sol' ),
+        		'singular_name'         => _x( 'Engineering Solutions', 'Post Type Singular Name', 'sol' ),
+        		'menu_name'             => __( 'Engineering Solutions', 'sol' ),
+        		'name_admin_bar'        => __( 'Engineering Solutions', 'sol' ),
+        		'archives'              => __( 'Engineering Archives', 'sol' ),
+    	    ),
+    		'supports'              => array( 
+				'title',
+				'editor',
+				'author',
+				'thumbnail',
+				'excerpt',
+				'page-attributes',
+				'theme-layouts',
+				'archive',
+				'arch-post'
+			),
+    		'taxonomies'            => array(
+				'category',
+				'post_tag'
+			),
+    		'hierarchical'          => true,
+    		'public'                => true,
+    		'show_ui'               => true,
+    		'show_in_menu'          => true,
+    		'menu_position'         => 5,
+    		'menu_icon'             => 'dashicons-networking',
+    		'show_in_admin_bar'     => true,
+    		'show_in_nav_menus'     => true,
+    		'can_export'            => true,
+    		'has_archive'           => true,
+    		'exclude_from_search'   => false,
+    		'publicly_queryable'    => true,
+    		'capability_type'       => 'page',
+	    )
+    );
+
+	register_post_type(
+		'staff_exec',
+		array(
+			'label'                 => __( 'Staffing and Executive', 'sol' ),
+			'labels'                => array(
+	    		'name'                  => _x( 'Staffing and Executive', 'sol' ),
+	    		'singular_name'         => _x( 'Staffing and Executive', 'sol' ),
+	    		'menu_name'             => __( 'Staffing and Executive', 'sol' ),
+	    		'name_admin_bar'        => __( 'Staffing and Executive', 'sol' ),
+	    		'archives'              => __( 'Staffing and Executive Archives', 'sol' ),
+		    ),
+			'supports'              => array(
+				'title',
+				'editor',
+				'author',
+				'thumbnail',
+				'excerpt',
+				'page-attributes',
+				'theme-layouts',
+				'archive',
+				'arch-post'
+			),
+    		'taxonomies'            => array(
+				'category',
+				'post_tag'
+			),
+			'hierarchical'          => true,
+			'public'                => true,
+			'show_ui'               => true,
+			'show_in_menu'          => true,
+			'menu_position'         => 5,
+			'menu_icon'             => 'dashicons-id-alt',
+			'show_in_admin_bar'     => true,
+			'show_in_nav_menus'     => true,
+			'can_export'            => true,
+			'has_archive'           => true,
+			'exclude_from_search'   => false,
+			'publicly_queryable'    => true,
+			'capability_type'       => 'page',
+		)
 	);
-
-	register_extended_post_type( 'eng_sol', array(
-
-	    # Add the post type to the site's main RSS feed:
-	    'show_in_feed' => true,
-		'supports'            => $sol_page_supports,
-
-	), array(
-
-	    # Override the base names used for labels:
-	    'singular' => 'Engineering Solutions',
-	    'plural'   => 'Engineering Solutions',
-	    'slug'     => 'engineering-solutions'
-
-	) );
-
-	register_extended_post_type( 'staff_exec', array(
-
-	    # Add the post type to the site's main RSS feed:
-	    'show_in_feed' => true,
-		'supports'            => $sol_page_supports,
-
-	), array(
-
-	    # Override the base names used for labels:
-	    'singular' => 'Staffing and Executive',
-	    'plural'   => 'Staffing and Executive',
-	    'slug'     => 'staffing-executive'
-
-	) );
 }
